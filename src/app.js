4d6de9d72cbf0f759caac980f61f33b87d45b11e
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2025-12-06 12:20:58 -0300

    add: create '/' endpoint

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import mainRoutes from './routes/index.js';
 
 app.use(json());
 
+app.use(mainRoutes);
+
 app.use('/api/v1', mainRoutes);
 
 export default app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,13 @@ import { Router } from 'express';
 const router = Router();
 import textRoutes from './text.routes.js';
 
+router.get('/', (req, res)=> {
+    res.status(200).json({
+        message: "Bem vindo ao StringCraft!",
+        version: "1.0"
+    })
+})
+
 router.use('/text', textRoutes);
 
 export default router;
